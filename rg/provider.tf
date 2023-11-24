be2e9df09c413955a2af639d91e9f1401f6a3125
kustoklasha <aturuka.kehinde@gmail.com>
2023-11-24 13:33:28 +0100

    create resource group

---
+++ rg/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+
+  backend "azurerm" {
+    subscription_id      = "2213e8b1-dbc7-4d54-8aff-b5e315df5e5b"
+    resource_group_name  = "1-c2d93d90-playground-sandbox"
+    storage_account_name = "tfstatebackendfiles"
+    container_name       = "tfstate"
+    key                  = "rg.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  subscription_id = var.subscription_id
+}
---
+++ rg/vars/1-c2d93d90-playground-sandbox.tfvars
@@ -0,0 +1,3 @@
+subscription_id         = "2213e8b1-dbc7-4d54-8aff-b5e315df5e5b"
+resource_group_name     = "1-c2d93d90-playground-sandbox"
+resource_group_location = "East US"
